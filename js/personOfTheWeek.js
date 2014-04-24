5a0ea7c80565e520c19b381c5d4d13448952695a
JoshuaChing <joshua-ching@hotmail.com>
2014-04-24 16:00:50 -0400

    Gathering data from IBC list

--- js/personOfTheWeek.js
+++ js/personOfTheWeek.js
@@ -2,3 +2,17 @@
  * Finds the person of the week and displays their required data from OICR
  * Written by Joshua Ching and Raunaq Suri
 */
+
+$.ajax({
+        type: "GET",
+        url: "files/IBC List_2014_v1.csv",
+        dataType: "text",
+        success: function(data) {
+            processIBClist(data);
+        }
+});
+
+function processIBClist(data){
+    console.log("process IBC list works");
+    console.log(data);
+}
