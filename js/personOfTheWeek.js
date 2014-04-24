f88650717d0c534a3a59f5e4a4372d680916738b
RaunaqSuri <raunaq.suri@hotmail.com>
2014-04-24 17:31:05 -0400

    Added Fisher-Yates (aka Knuth) shuffle

--- js/personOfTheWeek.js
+++ js/personOfTheWeek.js
@@ -21,13 +21,42 @@ function processIBClist(data){
     
     
 }
+
 //Gets all the names of the people
+var names = [];
 var csvLines = csvList.split("\n");
+
 for( var i = 0; i < csvLines.length; i++){
     var csvData = csvLines[i].split(",");
-    console.log(csvData[1]);
-    
+    names.push(csvData[1].trim()); 
+    //console.log(names[i]);
 }
+
+//Code taken from https://github.com/coolaj86/knuth-shuffle
+function shuffle(array) {
+  var currentIndex = array.length
+    , temporaryValue
+    , randomIndex
+    ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+names = shuffle(names);
+console.log(names);
 
 var person; //insert name of the person of the week here
 
